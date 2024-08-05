fc3e7758cc9d3616b8f678d3c0ccff9f99148b49
pfaffelh <p.p@stochastik.uni-freiburg.de>
2024-08-05 10:53:24 +0200

    changed typo in f-string3

--- import_old/import_lehrveranstaltungen.py
+++ import_old/import_lehrveranstaltungen.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 
-semesters = ["ws0607", "ss07", "ws0708", "ss08", "ws0809", "ss09", "ws0910", "ss10", "ws1011", "ss11", "ws1112", "ss12", "ws1213", "ss13", "ws1314", "ss14", "ws1415", "ss15", "ws1516", "ss16", "ws1617", "ss17", "ws1718", "ss18", "ws1819", "ss19", "ws1920", "ss20", "ws2021", "ss21", "ws2122", "ss22", "ws2223", "ss23", "ws2324", "ss24", "ws2425"]
+semesters = ["ws0607", "ss07", "ws0708", "ss08", "ws0809", "ss09", "ws0910", "ss10", "ws1011", "ss11", "ws1112", "ss12", "ws1213", "ss13", "ws1314", "ss14", "ws1415", "ss15", "ws1516", "ss16", "ws1617", "ss17", "ws1718", "ss18", "ws1819", "ss19", "ws1920", "ss20", "ws2021", "ss21", "ws2122", "ss22", "ws2223", "ss23", "ws2324", "ss24"]
 
 for semester in semesters:
     print(semester)
@@ -13,5 +13,5 @@ for semester in semesters:
     print(content)
     content['class'] = "container"
     filenames = [f"lehrveranstaltungen/{semester}.html"]    
-    with open("templates/"+filenames[0], "w") as file:
+    with open("../templates/"+filenames[0], "w") as file:
         file.write(content.prettify())
