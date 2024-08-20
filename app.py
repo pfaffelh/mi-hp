b4af1ee598e8759c04518f2be1208e763738d84b
pfaffelh <p.p@stochastik.uni-freiburg.de>
2024-08-20 23:53:24 +0200

    hello world -

--- app.py
+++ app.py
@@ -75,10 +75,6 @@ locale.setlocale(locale.LC_ALL, "de_DE.UTF8")
 ## Home page ##
 ###############
 
-@app.route("/")
-def showverybase():
-    return "Hello, world!"
-
 @app.route("/nlehre/test/")
 @app.route("/nlehre/test/<lang>/")
 @app.route("/nlehre/test/<lang>/<dtstring>")
