334ef96cf5a5eaf31502e50ab33ae2298b82b2f4
pfaffelh <p.p@stochastik.uni-freiburg.de>
2025-02-07 13:45:31 +0100

    merged wp_person8

--- utils/util_person.py
+++ utils/util_person.py
@@ -41,7 +41,7 @@ def get_person_data():
         # Verbindung beenden
         conn.unbind()
     except:
-        with open("static/data/ldap.json", 'r') as file:
+        with open("/usr/local/lib/mi-hp/static/data/ldap.json", 'r') as file:
             res = json.load(file)
 
     trans = {
