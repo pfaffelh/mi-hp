22bb34374cc9295e4b8a9148e2a158be5596a078
pfaffelh <pfaffelh@gmail.com>
2025-05-19 16:20:19 +0200

    added aktuell

--- app.py
+++ app.py
@@ -342,6 +342,10 @@ def showlehrveranstaltungenbase(lang="de"):
 @app.route("/nlehre/<lang>/lehrveranstaltungen/<semester>/<studiengang>")
 @app.route("/nlehre/<lang>/lehrveranstaltungen/<semester>/<studiengang>/<modul>")
 def showlehrveranstaltungen(lang, semester, studiengang = "", modul = ""):
+    if semester == "aktuell":
+        semester = vvz.get_current_semester_kurzname()
+    if semester == "naechstes":
+        semester = vvz.next_semester_kurzname(vvz.get_current_semester_kurzname())
     semesterliste_alt = ["ws0607", "ss07", "ws0708", "ss08", "ws0809", "ss09", "ws0910", "ss10", "ws1011", "ss11", "ws1112", "ss12", "ws1213", "ss13", "ws1314", "ss14", "ws1415", "ss15", "ws1516", "ss16", "ws1617", "ss17", "ws1718", "ss18", "ws1819", "ss19", "ws1920", "ss20", "ws2021", "ss21", "ws2122", "ss22", "ws2223", "ss23", "ws2324", "ss24"]
     if semester in semesterliste_alt:
         filenames = [f"lehrveranstaltungen/{semester}.html"]
